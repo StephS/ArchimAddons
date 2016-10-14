4f5135d28834cb85f3cda54cf1a20e354a65dfb3
tonokip <tonokip@gmail.com>
2016-10-14 14:17:26 -0500

    update arduino variant files for archim

--- variants/archim/variant.h
+++ variants/archim/variant.h
@@ -112,7 +112,7 @@ extern "C"{
 #define PIN_SPI_MOSI         (75u)
 #define PIN_SPI_MISO         (74u)
 #define PIN_SPI_SCK          (76u)
-#define BOARD_SPI_SS0        (10u)
+#define BOARD_SPI_SS0        (77u) //(10u)
 #define BOARD_SPI_SS1        (4u)
 #define BOARD_SPI_SS2        (52u)
 #define BOARD_SPI_SS3        PIN_SPI_SS3
@@ -223,7 +223,7 @@ static const uint8_t CAN1TX = 89;
  */
 #define PWM_INTERFACE		PWM
 #define PWM_INTERFACE_ID	ID_PWM
-#define PWM_FREQUENCY		1000
+#define PWM_FREQUENCY		31000
 #define PWM_MAX_DUTY_CYCLE	255
 #define PWM_MIN_DUTY_CYCLE	0
 #define PWM_RESOLUTION		8
@@ -233,7 +233,7 @@ static const uint8_t CAN1TX = 89;
  */
 #define TC_INTERFACE        TC0
 #define TC_INTERFACE_ID     ID_TC0
-#define TC_FREQUENCY        1000
+#define TC_FREQUENCY        100000
 #define TC_MAX_DUTY_CYCLE   255
 #define TC_MIN_DUTY_CYCLE   0
 #define TC_RESOLUTION		8
